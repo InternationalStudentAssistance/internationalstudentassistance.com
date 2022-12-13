539caaeb2952b11d21245fbbd91cabaa036e83b8
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-13 17:55:58 +1100

    add document head

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -11,6 +11,7 @@
  *
  */
 import { renderToStream, RenderToStreamOptions } from '@builder.io/qwik/server';
+import { DocumentHead } from "@builder.io/qwik-city";
 import { manifest } from '@qwik-client-manifest';
 import Root from './root';
 
@@ -25,3 +26,14 @@ export default function (opts: RenderToStreamOptions) {
     },
   });
 }
+
+export const head: DocumentHead = {
+  title: "International Student Assistance",
+  meta: [
+    {
+      name: "description",
+      content:
+        "The most helpful place on the internet for international students.",
+    },
+  ],
+};
